30970d43a000190295d04ea208078fc983602914
kiki0023 <kiarafer2302@gmail.com>
2025-07-13 01:29:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,68 @@
 const cursos = [
-  { nombre: "Comunicación", ciclo: 1 },
-  { nombre: "Matemática I", ciclo: 1 },
-  { nombre: "Historia", ciclo: 1 },
-  { nombre: "Contabilidad", ciclo: 2 },
-  { nombre: "Estadística", ciclo: 2 },
-  { nombre: "Marketing", ciclo: 3 },
+  {
+    "nombre": "ENGLISH I",
+    "ciclo": 1,
+    "prerequisito": null
+  },
+  {
+    "nombre": "ENGLISH II",
+    "ciclo": 2,
+    "prerequisito": "ENGLISH I"
+  },
+  {
+    "nombre": "MATEM\u00c1TICA",
+    "ciclo": 1,
+    "prerequisito": null
+  },
+  {
+    "nombre": "ESTAD\u00cdSTICA DESCRIPTIVA E INFERENCIA ESTAD\u00cdSTICA",
+    "ciclo": 3,
+    "prerequisito": "MATEM\u00c1TICA"
+  },
+  {
+    "nombre": "COSTOS DE ALIMENTOS Y BEBIDAS",
+    "ciclo": 3,
+    "prerequisito": "OPERACIONES Y SERVICIO DE ALIMENTOS Y BEBIDAS"
+  },
+  {
+    "nombre": "OPERACIONES Y SERVICIO DE ALIMENTOS Y BEBIDAS",
+    "ciclo": 2,
+    "prerequisito": "FUNDAMENTOS DE LA INDUSTRIA DE LA HOSPITALIDAD"
+  }
 ];
 
-const contenedor = document.querySelector(".malla");
+const estadoCursos = {};
 
-cursos.forEach(curso => {
-  const div = document.createElement("div");
-  div.className = "curso";
-  div.innerText = `${curso.nombre}\n(Ciclo ${curso.ciclo})`;
+function renderMalla() {
+    const container = document.querySelector(".malla");
+    container.innerHTML = "";
 
-  div.addEventListener("click", () => {
-    alert(`Curso: ${curso.nombre}\nCiclo: ${curso.ciclo}`);
-  });
+    cursos.forEach(curso => {
+        const div = document.createElement("div");
+        div.className = "curso";
+        div.innerText = `${curso.nombre}\n(Ciclo ${curso.ciclo})`;
+        div.dataset.nombre = curso.nombre;
+        div.dataset.prereq = curso.prerequisito || "Sin prerrequisito";
 
-  contenedor.appendChild(div);
-});
+        const prereq = curso.prerequisito;
+        if (!prereq || estadoCursos[prereq]) {
+            div.classList.add("activo");
+            div.style.cursor = "pointer";
+        }
+
+        if (estadoCursos[curso.nombre]) {
+            div.classList.add("completado");
+        }
+
+        div.addEventListener("click", () => {
+            if (div.classList.contains("activo")) {
+                estadoCursos[curso.nombre] = true;
+                renderMalla();
+            }
+        });
+
+        container.appendChild(div);
+    });
+}
+
+renderMalla();
